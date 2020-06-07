7bcef83be5176a9ff6a2a9ebc17076f20c949db8
Aleks8 <aleks253545@google.com>
2020-06-07 16:21:55 +0300

    rel5

--- js/heat.js
+++ js/heat.js
@@ -721,7 +721,7 @@ return heatmapFactory;
     localStorage.setItem('visit',true);
   }
 
-  document.querySelector('body').onclick = function(ev) {
+  document.querySelector('.heatmapContainer').onclick = function(ev) {
     heatmapInstance.addData({
       x: ev.pageX,
       y: ev.pageY,
